3500a497329765075b3a6840c378af1d2c764c55
doubleck <hankckh@gmail.com>
2016-03-14 22:39:36 -0400

    Remove the space in file name since rstudio has bug with space.
    And only deal with pdf files in the folder.

---
+++ rename_pdf.R
@@ -0,0 +1,19 @@
+install.packages("pdftools")
+library(pdftools)
+
+##set the folder path (Make this path you PDF file)
+setwd("F:/Dropbox New/Dropbox/Life long study/Ph.D/TRR Paper")
+##Retrieve PDF file name
+file.name<-list.files(pattern="\\.pdf$")
+##Write function for name swapping
+renamePDF<-function(myPDF){
+  subject<-(pdf_info(myPDF)$keys$Subject)
+  newsubject<-gsub(".","-",subject) #substitute weird stuff in file name
+  file.rename(from=myPDF,to=paste0(newsubject,".pdf"))
+}
+
+## Run the rename to all files
+sapply(file.name,renamePDF)
+
+##Done Yeah!!!!! Ling give me five!~~~~
+
