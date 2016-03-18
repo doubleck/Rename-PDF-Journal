ee14c7c9e63caf4ae45c6a1ff5b2de40d4fb2f04
Jung-Han Wang <hankckh@gmail.com>
2016-03-18 10:52:56 -0400

    Update rename_pdf.R
    Change description in the code

--- rename_pdf.R
+++ rename_pdf.R
@@ -3,18 +3,18 @@ library(pdftools)
 
 ##set the folder path (Make this path you PDF file)
 setwd("F:/Dropbox New/Dropbox/Life long study/Ph.D/TRR Paper")
-##Retrieve PDF file name
+##Retrieve the PDF file names
 file.name<-list.files(pattern="\\.pdf$")
 myPDF<-file.name[1]
-##Write function for name swapping
+##Write the function for the name swapping
 renamePDF<-function(myPDF){
   subject<-(pdf_info(myPDF)$keys$Subject)
-  newsubject<-gsub("…","-",subject) #substitute weird stuff in file name
+  newsubject<-gsub("…","-",subject) #substitute the weird stuff in file name
   file.rename(from=myPDF,to=paste0(newsubject,".pdf"))
 }
 
 ## Run the rename to all files
 sapply(file.name,renamePDF)
 
-##Done Yeah!!!!! Ling give me five!~~~~
+##Done Yeah!!!!! Ling, give me five!~~~~
 
